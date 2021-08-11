60ec6d2c2d6479ba4f62043ccf7940d2c2c071b0
maciekekseke96 <maciek20145c@wp.pl>
2021-08-11 12:26:38 +0200

    Basic axios structure prepared for fetching cat categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React, { Component } from 'react';
+import { getCategories } from './axios_request';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Here will be The Cat Tinder</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+  setCategories = async () => {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  };
+  componentDidMount() {
+    this.setCategories();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Here will be The Cat Tinder</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/axios_request/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.thecatapi.com/v1/',
+});
+
+let config = {
+  headers: { 'x-api-key': 'd3b8756c-45d8-471b-ba31-64d0422348ba' },
+};
+
+const getData = async (path) => {
+  const data = await api.get(`/${path}`, config).then((resp) => resp.data);
+  return data;
+};
+
+export const getCategories = () => getData('categories');
