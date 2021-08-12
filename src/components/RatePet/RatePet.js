7dcba72827323345e6bf10967283ab0564e0dcdf
maciekekseke96 <maciek20145c@wp.pl>
2021-08-12 15:20:54 +0200

    Logic of rendering images works

--- src/components/RatePet/RatePet.js
+++ src/components/RatePet/RatePet.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import RatingButton from '../RatingButton/RatingButton';
 import { ReactComponent as PetIt } from './../../assets/Pet.svg';
 import { ReactComponent as SkipIt } from './../../assets/Skip.svg';
 import { ReactComponent as NoPet } from './../../assets/DontPet.svg';
 import './RatePet.scss';
 
-const RatePet = () => {
+const RatePet = ({votingDetails, images}) => {
+
+  const votedCatsNumber = votingDetails.voted;
+
+  const styleObject = images?{backgroundImage:`url(${images[votedCatsNumber].url}`}:{};
+
   return (
     <div className="ratePet_container">
-      <div className="petImage"></div>
+      <div className="petImage" style={styleObject}></div>
       <div className="ratingButtons_container">
         <div className="ratingButtons">
-        <p className="title">Cat 4/10</p>
-          <RatingButton icon={NoPet} description="Don't pet it!" color={'red'}/>
-          <RatingButton icon={SkipIt} description="Skip it!"  color={'grey'} />
-          <RatingButton icon={PetIt} description="Pet it!"  color={'green'}/>
+          <p className="title">{`Cat ${votedCatsNumber + 1}/10`}`</p>
+          <RatingButton
+            icon={NoPet}
+            description="Don't pet it!"
+            color={'red'}
+          />
+          <RatingButton icon={SkipIt} description="Skip it!" color={'grey'} />
+          <RatingButton icon={PetIt} description="Pet it!" color={'green'} />
         </div>
       </div>
     </div>
   );
 };
 
-export default RatePet;
+const mapStateToProps = (state) => ({
+  images: state.images.images,
+  votingDetails: state.votingDetails,
+});
+
+export default connect(mapStateToProps, null)(RatePet);
